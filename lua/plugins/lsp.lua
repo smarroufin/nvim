116491502bbeb6f27c0b3d7dac12d85f08af30ac
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2025-04-10 15:21:48 +0200

    chore(lsp): switch back to lazydev

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,34 +1,6 @@
 -- `:Mason`, `:MasonInstall package`
 -- https://github.com/neovim/nvim-lspconfig/tree/master/lua/lspconfig/configs
 local servers = {
-  lua_ls = {
-    on_init = function(client)
-      if client.workspace_folders then
-        local path = client.workspace_folders[1].name
-        if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-          return
-        end
-      end
-
-      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-        runtime = {
-          version = 'LuaJIT',
-        },
-        workspace = {
-          checkThirdParty = false,
-          library = {
-            vim.env.VIMRUNTIME,
-            '${3rd}/luv/library',
-          },
-        },
-      })
-    end,
-    settings = {
-      Lua = {
-        -- https://luals.github.io/
-      },
-    },
-  },
   ts_ls = {
     filetypes = {
       'javascript',
