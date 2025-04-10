b55069e2d7771ce2722bce9545cf024d0491727f
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2025-04-10 23:23:34 +0200

    fix: vue syntax highlight

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      {
+        'nvim-treesitter/nvim-treesitter-context',
+        opts = {
+          mode = 'cursor',
+          max_lines = 3,
+          multiline_threshold = 1,
+        },
+      },
+    },
+    opts = {
+      ensure_installed = {
+        'css',
+        'html',
+        'javascript',
+        'lua',
+        'query',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'vue',
+      },
+      sync_install = false,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+    main = 'nvim-treesitter.configs',
+  },
+}
