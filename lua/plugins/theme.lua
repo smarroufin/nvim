de583ef713ec95d517201091d6de864577de116e
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2025-04-14 23:15:56 +0200

    chore: update theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,10 @@
 return {
   {
-    'folke/tokyonight.nvim',
+    'EdenEast/nightfox.nvim',
     lazy = false,
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme('tokyonight-night')
-      vim.cmd.hi('Comment gui=none')
+      vim.cmd.colorscheme('carbonfox')
     end,
   },
 }
